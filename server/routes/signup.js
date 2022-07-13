e5e87b6ac937c323c1deca18e7a21fb091835ebe
Joshua Perez <joshuaetperez@gmail.com>
2022-07-12 19:05:29 -0700

    When a user signs up, a verification email is now sent to their email account

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pool = require('../db');
 const bcrpyt = require('bcrypt');
+const nodemailer = require('nodemailer');
+const {v4: uuidv4} = require('uuid');
 const registerSchema = require('../schema/register-schema');
 const validateRequestSchema = require('../middlewares/validate-request-schema');
 
@@ -15,9 +17,39 @@ signupRouter.post(
     try {
       const {email, password} = req.body;
       const hashedPassword = await bcrpyt.hash(password, 10);
+      const uuid = uuidv4();
       const newUser = await pool.query(
-        'INSERT INTO users (email, password) VALUES($1, $2) RETURNING *',
-        [email, hashedPassword]
+        'INSERT INTO users (email, password, uuid) VALUES($1, $2, $3) RETURNING *',
+        [email, hashedPassword, uuid]
+      );
+
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'backloglibrary@gmail.com',
+          pass: process.env.BACKLOG_EMAIL_PW,
+        },
+      });
+
+      const output = `
+        <h3>New Account Verification</h3>
+        <a href="http://localhost:5000/user/verification/${uuid}">Please click here to verify your email account</a>
+        <p>Ignore this email if you did not create a Backlog Library account.</p>
+      `;
+      transporter.sendMail(
+        {
+          from: '"Backlog Library" <backloglibrary@gmail.com>',
+          to: email,
+          subject: 'New Account Verification',
+          html: output,
+        },
+        (err, info) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Message sent: %s', info.messageId);
+          }
+        }
       );
       res.json(newUser.rows[0]);
     } catch (err) {
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const pool = require('../db');
 
 const userRouter = express.Router();
 
 userRouter.get('/user', (req, res) => {
   res.send(req.user);
+});
+
+userRouter.get('/user/verification/:uuid', async (req, res) => {
+  try {
+    await pool.query('UPDATE users SET verified = TRUE WHERE uuid = $1', [
+      req.params.uuid,
+    ]);
+    res.redirect('http://localhost:3000/login');
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = userRouter;
