e8bc33d93e031271569bd9d67a3de1f19a800359
Joshua Perez <joshuaetperez@gmail.com>
2022-08-01 16:50:11 -0700

    Changed port number if not in production

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
 const express = require('express');
