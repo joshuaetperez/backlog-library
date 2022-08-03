55db466357170ad328080a2b481fc715e140e6df
Joshua Perez <joshuaetperez@gmail.com>
2022-08-02 18:48:45 -0700

    Separating client and server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const {CLIENT_URL} = require('./http');
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
-const path = require('path');
 const app = express();
 const cors = require('cors');
 const passport = require('passport');
@@ -25,16 +24,9 @@ app.use(
     saveUninitialized: false,
   })
 );
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api', router);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
