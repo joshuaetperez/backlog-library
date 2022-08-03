b9f8c48841e85a3adb184d5c7e032ffb124b0d5a
Joshua Perez <joshuaetperez@gmail.com>
2022-08-03 11:12:28 -0700

    Added react build to server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {CLIENT_URL} = require('./http');
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const passport = require('passport');
@@ -26,7 +27,17 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+// React build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use('/api', router);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
